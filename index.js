035c8d0c4f7d369bad4497734c087e62385529c2
anton-varenyuk <anton.varenyuk@avenga.com>
2021-06-30 18:23:12 +0300

    npm satrt

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('lol kek');
+const http = require('http');
+
+const hostname = 'localhost';
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
