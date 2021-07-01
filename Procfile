aa2275e520aa13d526e4e498c18fc351a297dbd3
anton-varenyuk <anton.varenyuk@avenga.com>
2021-07-01 18:39:28 +0300

    npm satrt

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: npm install
-worker: node index.js
+web: node index.js
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('./', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`, 'lol')
+    console.log(`Example app listening at ${port}`, 'lol')
 })
 
